1557d69eca5a2a686968a6c6dda5e4cc8cb9f06d
jebewa <timmunters@hotmail.com>
2017-07-21 12:09:08 +0200

    Update imageviewer.js

--- viewer/imageviewer.js
+++ viewer/imageviewer.js
@@ -217,7 +217,7 @@ else if(anjouChoice == "fifth_one") {
     document.getElementById("first_des").style.display = "none";
     document.getElementById("second_des").style.display = "block";
 }
-else if(anjouChoice == "sixth_one") {
+if(anjouChoice == "sixth_one") {
     document.getElementById("thumbs6").style.display = "block";
     document.getElementById('im').src = document.getElementById('sixth_thumb').src;
     document.getElementById("first_des").style.display = "none";
